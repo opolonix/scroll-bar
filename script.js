aa0ba75690f2c4b56af13c0bfef3ea6f291d8081
MISHA <misha90638@gmail.com>
2023-04-06 22:44:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ class ScrollBar {
     load() {
         var caret = document.querySelector('#scroll__bar__caret')
         var caret_container = document.querySelector('#scroll__bar__container')
+        var caret_position = window.pageYOffset || document.documentElement.scrollTop;
         var drag_caret = false
         document.querySelector('#scroll__bar__container').style.display = 'block'
         document.addEventListener('mousemove', (event) => {
@@ -22,8 +23,8 @@ class ScrollBar {
                 caret.style.width = '16px'
             }
             else caret.style.width = '2px'
-            if (drag_caret == true && event.screenY - 74 > 0 && event.screenY - 74 < window.innerHeight){
-                var scroll_caret = (event.screenY - 74) / (window.innerHeight) * (window.innerHeight - caret.offsetHeight - 4)
+            if (drag_caret == true && event.screenY > 0 && event.screenY < window.innerHeight){
+                var scroll_caret = event.screenY - caret.offsetHeight - 2
                 caret_container.style = `padding-top: ${scroll_caret}px`
                 // window.scrollTo(0, (event.screenY) / window.innerHeight * scrollHeight);
             }
@@ -35,7 +36,13 @@ class ScrollBar {
                 drag_caret = true
                 caret.style.transition = '.2s'
                 caret.style.backgroundColor = '#fafafa70'
-                var scroll_caret = (event.screenY - 72) / window.innerHeight * (window.innerHeight - caret.offsetHeight - 4)
+                if (event.srcElement.id == 'scroll__bar__caret'){
+                    var scroll_caret = event.screenY - caret.offsetHeight - 2
+                }
+                else {
+                    var scroll_caret = event.screenY - caret.offsetHeight - 2
+                }
+                
                 caret_container.style = `padding-top: ${scroll_caret}px`
                 // window.scrollTo(0, (event.screenY) / window.innerHeight * scrollHeight);
             }
@@ -51,6 +58,7 @@ class ScrollBar {
         })
         document.addEventListener('scroll', (event) => {
             var scroll_top = window.pageYOffset || document.documentElement.scrollTop;
+            caret_position = scroll_top
             var scroll_caret = scroll_top / (scrollHeight - window.innerHeight) * (window.innerHeight - caret.offsetHeight - 4)
             caret_container.style = `padding-top: ${scroll_caret}px`
         })
